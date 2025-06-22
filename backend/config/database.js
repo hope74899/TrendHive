d106105c203957fe3c2c2ccafda73ba94be08589
Muhammad Rashid <m.rashid2768@gmail.com>
2025-06-22 16:13:15 +0500

    database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config();
-
 const connection = async () => {
     try {
         const connect = await mongoose.connect(process.env.URL)
