3ff56843e467a546d2e4138259af59fd4b2a86a4
Muhammad Rashid <m.rashid2768@gmail.com>
2025-06-22 19:38:20 +0500

    app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const path =require('path')
 
 
 const app = express();
-app.use("/public", express.static(path.join(__dirname, "public")));
+// app.use("/public", express.static(path.join(__dirname, "public")));
 const corsOptions = {
     origin: process.env.FRONTENDPATH,
     methods: "GET, POST, PATCH, PUT, DELETE, HEAD",
